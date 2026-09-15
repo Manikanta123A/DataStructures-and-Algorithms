09bcdbdbbdef5f86df0f653398155a59bede2c82
Manikanta123A <151040129+Manikanta123A@users.noreply.github.com>
2026-09-16 00:49:02 +0530

    Time: 0 ms (100%), Space: 19.3 MB (67.67%) - LeetHub

---
+++ 0672-bulb-switcher-ii/0672-bulb-switcher-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def flipLights(self, n: int, presses: int) -> int:
+
+        if presses == 0:
+            return 1
+
+        if n == 1:
+            return 2
+
+        if n == 2:
+            return min(4, presses + 2)
+
+
+        if presses == 1:
+            return 4
+        elif presses == 2:
+            return 7
+        else:
+            return 8
