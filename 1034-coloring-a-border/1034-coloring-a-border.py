ff5ca412efa3e940878336b751c97cef59cb8be1
Manikanta123A <151040129+Manikanta123A@users.noreply.github.com>
2026-09-26 00:15:02 +0530

    Time: 1 ms (42.09%), Space: 20.4 MB (39.7%) - LeetHub

--- 1034-coloring-a-border/1034-coloring-a-border.py
+++ 1034-coloring-a-border/1034-coloring-a-border.py
@@ -6,29 +6,24 @@ class Solution:
         m, n = len(grid), len(grid[0])
         original = grid[row][col]
 
-        visited = [[False] * n for _ in range(m)]
+        visited = set()
         borders = []
 
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
         def dfs(r, c):
-            visited[r][c] = True
-
-            is_border = False
+            visited.add((r,c))
 
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
 
                 if nr < 0 or nr >= m or nc < 0 or nc >= n:
-                    is_border = True
+                    borders.append((r, c))
                 elif grid[nr][nc] != original:
-                    is_border = True
-                elif not visited[nr][nc]:
+                    borders.append((r, c))
+                elif (nr,nc) not in visited:
                     dfs(nr, nc)
-
-            if is_border:
-                borders.append((r, c))
-
+                
         dfs(row, col)
 
         for r, c in borders:
